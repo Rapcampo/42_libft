d8a940ced4ea018a1959eb438885aecee0a243a0
Rapcampo <142260510+Rapcampo@users.noreply.github.com>
2023-10-13 00:00:15 +0100

    bonus funcs no header

---
+++ ft_lstmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*node;
+
+	new = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	while (lst != NULL)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, node);
+		lst = lst->next;
+	}
+	return (new);
+}
